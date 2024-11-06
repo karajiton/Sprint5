c76017773b2b626c2b3c560a38e12066ad1305a9
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-11-06 10:48:56 +0100

    prueba

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php 
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlayerController;
+
+Route::post('/players', [PlayerController::class, 'createPlayer']);
+Route::put('/players/{id}', [PlayerController::class, 'updatePlayer']);
+Route::post('/players/{id}/games', [PlayerController::class, 'rollDice']);
+Route::delete('/players/{id}/games', [PlayerController::class, 'deleteGames']);
+Route::get('/players', [PlayerController::class, 'listPlayers']);
+Route::get('/players/{id}/games', [PlayerController::class, 'listGames']);
+Route::get('/players/ranking', [PlayerController::class, 'ranking']);
+Route::get('/players/ranking/loser', [PlayerController::class, 'worstPlayer']);
+Route::get('/players/ranking/winner', [PlayerController::class, 'bestPlayer']);
