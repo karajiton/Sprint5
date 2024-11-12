d123496d7a0a7e527eaef08bdf034f38cbb67972
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-11-12 10:58:12 +0100

    proteger las rutas de la api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayerController;
 
+
+Route::middleware('auth:api')->group(function () {
 Route::post('/players', [PlayerController::class, 'createPlayer']);
 Route::put('/players/{id}', [PlayerController::class, 'updatePlayer']);
 Route::post('/players/{id}/games', [PlayerController::class, 'rollDice']);
@@ -12,3 +14,4 @@ Route::get('/players/{id}/games', [PlayerController::class, 'listGames']);
 Route::get('/players/ranking', [PlayerController::class, 'ranking']);
 Route::get('/players/ranking/loser', [PlayerController::class, 'worstPlayer']);
 Route::get('/players/ranking/winner', [PlayerController::class, 'bestPlayer']);
+});
