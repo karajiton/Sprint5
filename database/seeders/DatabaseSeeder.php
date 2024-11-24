e99a3bf35606c2ace47415068f8c4d3edb8ab785
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-11-24 22:01:07 +0100

    separando las rutas por roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    
     /**
      * Seed the application's database.
      */
@@ -19,5 +20,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call(RolesAndPermissionsSeeder::class);
+
     }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -6,29 +6,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PlayerController;
 
 Route::post("register", [AuthenticationController::class, "register"]);
+Route::post("login", [AuthenticationController::class, "login"]);
 Route::middleware('auth:api')->group(function () {
 Route::post('/players', [PlayerController::class, 'createPlayer']);
 Route::put('/players/{id}', [PlayerController::class, 'updatePlayer']);
 Route::post('/players/{id}/games', [PlayerController::class, 'rollDice']);
 Route::delete('/players/{id}/games', [PlayerController::class, 'deleteGames']);
-Route::get('/players', [PlayerController::class, 'listPlayers']);
 Route::get('/players/{id}/games', [PlayerController::class, 'listGames']);
-Route::get('/players/ranking', [PlayerController::class, 'ranking']);
-Route::get('/players/ranking/loser', [PlayerController::class, 'worstPlayer']);
-Route::get('/players/ranking/winner', [PlayerController::class, 'bestPlayer']);
+
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\API'],function()
-{
-    // --------------- register and login ----------------//
-    Route::controller(AuthenticationController::class)->group(function () {
-        
-        Route::post('register', 'register')->name('register');
-        Route::post('login', 'login')->name('login');
-        Route::post('login/out', 'logOut')->name('login/out');
-    });
-    // ------------------ get data ----------------------//
-    Route::controller(AuthenticationController::class)->group(function () {
-        Route::get('get-user', 'userInfo')->middleware('auth:api')->name('get-user');
-    });
+Route::middleware('auth:api', 'role:admin')->group(function (){
+Route::get('/players', [PlayerController::class, 'listPlayers']);
+    Route::get('/players/ranking', [PlayerController::class, 'ranking']);
+    Route::get('/players/ranking/loser', [PlayerController::class, 'worstPlayer']);
+    Route::get('/players/ranking/winner', [PlayerController::class, 'bestPlayer']);
 });
