791518a35a6f286afe454329173977890b8fe816
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-11-26 19:52:28 +0100

    ya me funciona los roles gracias a que agregue protected string $guard_name = 'web'; este comando al modelo

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected function getDefaultGuardName(): string { return 'api'; }
     protected $fillable = [
         'name',
         'email',
